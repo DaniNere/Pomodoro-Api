6a6ad31d8ac253bad7393812bb9c24436f19a955
DaniNere <danisilvanere@gmail.com>
2024-04-04 13:47:51 -0300

    🔧countdown timerController

--- src/main/kotlin/com/pomodoro/controller/timerController.kt
+++ src/main/kotlin/com/pomodoro/controller/timerController.kt
@@ -1,5 +1,6 @@
 package com.pomodoro.controller
 
+import com.pomodoro.service.PomodoroTimer
 import org.springframework.web.bind.annotation.*
 import com.pomodoro.service.TimerConfigService
 import java.util.*
@@ -7,7 +8,9 @@ import kotlin.concurrent.schedule
 
 @RestController
 @RequestMapping("/timer")
-class TimerController(private val timerConfigService: TimerConfigService) {
+
+class TimerController(private val timerConfigService: TimerConfigService,
+                      private val pomodoroTimer: PomodoroTimer) {
     private var workTimer: Timer? = null
     private var breakTimer: Timer? = null
     @GetMapping("/start")
@@ -17,6 +20,7 @@ class TimerController(private val timerConfigService: TimerConfigService) {
         workTimer?.schedule(workTime) {
             // Lógica para quando o temporizador de trabalho terminar
             println("Temporizador de trabalho encerrado")
+            pomodoroTimer.onWorkTimerFinished() // Chame o método na classe PomodoroTimer
         }
         return "Timer started with work time of ${timerConfigService.getWorkTime()} minutes"
     }
@@ -24,15 +28,16 @@ class TimerController(private val timerConfigService: TimerConfigService) {
     @GetMapping("/pause")
     fun pauseTimer(): String {
         // Lógica para pausar o temporizador Pomodoro
+        pomodoroTimer.pauseTimer()//pausa o cronometro
         return "Timer paused"
     }
 
     @GetMapping("/stop")
     fun stopTimer(): String {
         // Lógica para parar o temporizador Pomodoro
+        pomodoroTimer.pauseTimer()//pausa o cronometro
         return "Timer stopped"
     }
-
     @PostMapping("/work-time/{minutes}")
     fun setWorkTime(@PathVariable minutes: Long): String {
         timerConfigService.setWorkTime(minutes)
